80c7d52fdaa41961093b9b806edc25f53112f02c
pacoorozco <paco@pacoorozco.info>
2014-06-19 14:00:47 +0200

    PSR-2 code guide

--- resources/lib/functions.php
+++ resources/lib/functions.php
@@ -106,7 +106,7 @@ function getRandomString($length = 10)
  * Checks if some error has done while uploading.
  * Checks if filetype is allowed or no.
  *
- * @param   string  $file_field     Name of file upload in html form
+ * @param   string  $fileField     Name of file upload in html form
  * @param   string  $destination    The directory where file will be moved
  * @param   array   $allowedTypes  An array containing tiletypes allowed
  *
@@ -123,7 +123,7 @@ function getRandomString($length = 10)
  * On 'false' is an error message string
  * On 'true' is the generated filename.
  */
-function uploadFile($file_field, $destination, $allowedTypes = array())
+function uploadFile($fileField, $destination, $allowedTypes = array())
 {
 
     // Default allowed list of file to be uploaded
@@ -137,13 +137,13 @@ function uploadFile($file_field, $destination, $allowedTypes = array())
 
     // Undefined | Multiple Files | $_FILES Corruption Attack
     // If this request falls under any of them, treat it invalid.
-    if (!isset($_FILES[$file_field]['error']) || is_array($_FILES[$file_field]['error'])) {
+    if (!isset($_FILES[$fileField]['error']) || is_array($_FILES[$fileField]['error'])) {
         // Invalid parameters
         return array(false , 'Invalid parameters');
     }
 
     // Check $file['error'] value.
-    switch ($_FILES[$file_field]['error']) {
+    switch ($_FILES[$fileField]['error']) {
         case UPLOAD_ERR_OK:
             break;
         case UPLOAD_ERR_NO_FILE:
@@ -161,7 +161,7 @@ function uploadFile($file_field, $destination, $allowedTypes = array())
     // Check MIME Type
     $finfo = new finfo(FILEINFO_MIME_TYPE);
     if (false === $ext = array_search(
-        $finfo->file($_FILES[$file_field]['tmp_name']),
+        $finfo->file($_FILES[$fileField]['tmp_name']),
         $allowedTypes,
         true
     )) {
@@ -177,7 +177,7 @@ function uploadFile($file_field, $destination, $allowedTypes = array())
         $ext
     );
 
-    if (!move_uploaded_file($_FILES[$file_field]['tmp_name'], $filename)) {
+    if (!move_uploaded_file($_FILES[$fileField]['tmp_name'], $filename)) {
         // Failed to move uploaded file
         return array(false, 'Failed to move uploaded file');
     }
@@ -246,11 +246,11 @@ function getHTMLMessages($messages)
     return implode(PHP_EOL, $htmlCode);
 }
 
-function getHTMLSelectOptions($available_options, $selected_option = '')
+function getHTMLSelectOptions($availableOptions, $selectedOption = '')
 {
     $htmlCode = array();
-    foreach ($available_options as $key => $value) {
-        if ($key == $selected_option) {
+    foreach ($availableOptions as $key => $value) {
+        if ($key == $selectedOption) {
             $htmlCode[] = '<option value="' . $key . '" selected="selected">' . $value . '</option>';
         } else {
             $htmlCode[] = '<option value="' . $key . '">' . $value . '</option>';
@@ -310,13 +310,13 @@ function getREQUESTVar($in, $default = false)
         return isset($_POST[$in]) ? getPOSTVar($in) : ( isset($_GET[$in]) ? getGETVar($in) : $default );
 }
 
-function getSanitizedInput($in, $force_slashes = 0, $maxLength = 0)
+function getSanitizedInput($in, $forceSlashes = 0, $maxLength = 0)
 {
 
     // If $in is array we process every value
     if (is_array($in)) {
         foreach ($in as &$element) {
-            $element = getSanitizedInput($element, $force_slashes = 0, $maxLength = 0);
+            $element = getSanitizedInput($element, $forceSlashes = 0, $maxLength = 0);
         }
         unset ($element);
     } else {
@@ -329,7 +329,7 @@ function getSanitizedInput($in, $force_slashes = 0, $maxLength = 0)
         }
 
         // Add slashes
-        if ($force_slashes) {
+        if ($forceSlashes) {
             $in = addslashes($in);
         }
 
